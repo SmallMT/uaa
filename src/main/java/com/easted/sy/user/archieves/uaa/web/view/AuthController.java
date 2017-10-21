03342e286953ca1ac456452eb5e5b430d75a06ad
liuyatao <liuyatao1992@gmail.com>
2017-10-21 19:06:41 +0800

    可打包

--- src/main/java/com/easted/sy/user/archieves/uaa/web/view/AuthController.java
+++ src/main/java/com/easted/sy/user/archieves/uaa/web/view/AuthController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import sun.jvm.hotspot.gc_implementation.parallelScavenge.PSOldGen;
 
 import javax.imageio.ImageIO;
 import javax.persistence.PreUpdate;
