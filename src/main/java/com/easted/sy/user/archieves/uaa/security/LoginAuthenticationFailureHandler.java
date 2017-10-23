6448353c89c1897852e2405371a50fab9a535634
liuyatao <liuyatao1992@gmail.com>
2017-10-23 11:51:53 +0800

    正式环境

--- src/main/java/com/easted/sy/user/archieves/uaa/security/LoginAuthenticationFailureHandler.java
+++ src/main/java/com/easted/sy/user/archieves/uaa/security/LoginAuthenticationFailureHandler.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 public class LoginAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
-    public static final String CODE_ERROR_URL = "http://localhost:9090/login?code_error";
-    public static final String EXPIRED_URL = "http://localhost:9090/login?expired";
-    public static final String LOCKED_URL = "http://localhost:9090/login?locked";
-    public static final String DISABLED_URL = "http://localhost:9090/login?disabled";
-    public static final String NotActivated="http://localhost:9090/login?NotActivated";
+    public static final String CODE_ERROR_URL = "http://199.224.20.102:9090/login?code_error";
+    public static final String EXPIRED_URL = "http://199.224.20.102:9090/login?expired";
+    public static final String LOCKED_URL = "http://199.224.20.102:9090/login?locked";
+    public static final String DISABLED_URL = "http://199.224.20.102:9090/login?disabled";
+    public static final String NotActivated="http://199.224.20.102:9090/login?NotActivated";
 
-    public static final String PASS_ERROR_URL = "http://localhost:9090/login?pass_error";
+    public static final String PASS_ERROR_URL = "http://199.224.20.102:9090/login?pass_error";
 
     public LoginAuthenticationFailureHandler() {
     }
